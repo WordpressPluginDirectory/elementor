686557155e1c7aea61e43c2b76625979cbb50f91
Justin Gardner <justin@rhynorater.com>
2026-05-26 06:01:42 -0500

    4.1.0

--- elementor/vendor/composer/installed.php
+++ elementor/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'elementor/elementor',
-        'pretty_version' => '4.00.x-dev',
-        'version' => '4.00.9999999.9999999-dev',
-        'reference' => '00258303329141ecfdb7c614549abd5b4d06b067',
+        'pretty_version' => '4.01.x-dev',
+        'version' => '4.01.9999999.9999999-dev',
+        'reference' => '07793f7c65cb1039ec3e8d123eb3e9fc8cda282c',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'elementor/elementor' => array(
-            'pretty_version' => '4.00.x-dev',
-            'version' => '4.00.9999999.9999999-dev',
-            'reference' => '00258303329141ecfdb7c614549abd5b4d06b067',
+            'pretty_version' => '4.01.x-dev',
+            'version' => '4.01.9999999.9999999-dev',
+            'reference' => '07793f7c65cb1039ec3e8d123eb3e9fc8cda282c',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
